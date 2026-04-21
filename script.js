ea39e9d97e4192adcf2f6ac1c29d0082fad9359c
pippo <you@example.com>
2026-04-21 18:54:36 +0200

    parte

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-// Inizializzazione mappa centrata su Roma
-var map = L.map('map').setView([41.9028, 12.4964], 13);
+const map = L.map('map').setView([41.9028, 12.4964], 13);
 
-// Tile layer OpenStreetMap
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
+
+// AGGIUNTA
+aggiungiMonumenti(map);
+
+document.getElementById("formMonumento").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    const lat = parseFloat(document.getElementById("lat").value);
+    const lng = parseFloat(document.getElementById("lng").value);
+
+    const nuovo = { lat, lng };
+
+    aggiungiMonumentoArray(nuovo);
+
+    L.marker([lat, lng]).addTo(map);
+});
